557414959d4b6c33c37251e8745db5ab99e54620
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amu.fr>
2026-04-09 23:54:32 +0200

    mise en local de tous les fichiers apiService

--- src/services/ApiOrder.php
+++ src/services/ApiOrder.php
@@ -3,8 +3,8 @@
 namespace App\services;
 
 class ApiOrder{
-    // private string $url = "http://localhost:3005/commandes";
-    private string $url = "http://localhost:8080/API-Commandes-1.0-SNAPSHOT/api/commandes";
+    private string $url = "http://localhost:3005/commandes";
+    //private string $url = "http://localhost:8080/API-Commandes-1.0-SNAPSHOT/api/commandes";
 
     public function getOrderByUserId(int $userId){
         $responseJson = file_get_contents($this->url . "?abonneId=".$userId);
